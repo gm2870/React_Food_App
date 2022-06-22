ba0cca11d800b3cddcb947f0264b0444a44c0558
gm2870 <alifarzanegan70@gmail.com>
2022-06-22 12:47:13 +0430

    added  useHttp hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios({
+        method: requestConfig.method || 'GET',
+        url: requestConfig.url || {},
+        data: JSON.stringify(requestConfig.data) || null,
+      });
+      applyData(response.data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  });
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
