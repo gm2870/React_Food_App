5132643c877b6f1c4083ba159429f3f7d9e70dcc
gm2870 <alifarzanegan70@gmail.com>
2022-06-22 12:47:57 +0430

    implemented fetch meals

---
+++ src/components/API/api.js
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  'https://react-food-app-8a2f6-default-rtdb.asia-southeast1.firebasedatabase.app';
+
+export const MEALS_URL = `${BASE_URL}/meals.json`;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,34 +1,28 @@
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from 'react';
+import useHttp from '../../hooks/use-http';
+import { MEALS_URL } from '../API/api';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
-  const melasList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState();
+  const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+  useEffect(() => {
+    const transFormMeals = (mealsObj) => {
+      const loadedMeals = [];
+      for (const key in mealsObj) {
+        loadedMeals.push(...mealsObj[key]);
+      }
+      setMeals(loadedMeals);
+    };
+    const config = {
+      url: MEALS_URL,
+      method: 'GET',
+    };
+    fetchMeals(config, transFormMeals);
+  }, []);
+
+  const melasList = meals?.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -39,9 +33,12 @@ const AvailableMeals = () => {
   ));
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>{melasList}</ul>
-      </Card>
+      {isLoading && <p className={classes['loading-text']}>Loading meals...</p>}
+      {melasList && (
+        <Card>
+          <ul>{melasList}</ul>
+        </Card>
+      )}
     </section>
   );
 };
